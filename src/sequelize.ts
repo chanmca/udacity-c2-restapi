55fe8e1280c0d33c9c36799e833445ff374f65ec
chanmca <chanhemgau@outlook.com>
2020-10-15 09:19:23 +0530

    Second Commit

---
+++ src/sequelize.ts
@@ -0,0 +1,16 @@
+import {Sequelize} from 'sequelize-typescript';
+import { config } from './config/config';
+
+const postgres = config.devpostgres;
+
+// Instantiate new Sequelize instance!
+export const sequelize = new Sequelize({
+  "username": postgres.username,
+  "password": postgres.password,
+  "database": postgres.database,
+  "host":     postgres.host,
+
+  dialect: 'postgres',
+  storage: ':memory:',
+});
+
